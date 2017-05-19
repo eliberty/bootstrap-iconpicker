7e4bf4faeb01162a44a7d7114caa7af7bcd5fc44
BENARD Patrick <pbeliberty@users.noreply.github.com>
2017-05-19 22:40:26 +0200

    Develop (#3)
    * add font-awesome v4.7
    
    * update license
    
    * Develop (#2)
    
    * Support 'container' popover option
    
    Added container to the option to allow the popover's container being passed from outside
    
    * Support 'container' popover option
    
    Added popover 'container' option to avoid the issue in which the popover does not follow the attached element.
    
    * updateArrows function fixed.
    
    Pagination goes negative values. Toggle button disabled property with toggling class.
    
    * Create iconset-ionicon-2.0.1.min.js
    
    * Always ressetting data for fixing cloning issue in construction
    
    https://github.com/victor-valencia/bootstrap-iconpicker/issues/63
    
    * Update bootstrap-iconpicker.min.js

--- bootstrap-iconpicker/js/bootstrap-iconpicker.js
+++ bootstrap-iconpicker/js/bootstrap-iconpicker.js
@@ -193,16 +193,16 @@
         var op = this.options;
         var total_pages = this.totalPages();
         if (page === 1) { 
-            op.table.find('.btn-previous').addClass('disabled');
+            op.table.find('.btn-previous').addClass('disabled').prop('disabled', true);
         }
         else {
-            op.table.find('.btn-previous').removeClass('disabled');
+            op.table.find('.btn-previous').removeClass('disabled').prop('disabled', false);
         }
         if (page === total_pages || total_pages === 0) { 
-            op.table.find('.btn-next').addClass('disabled');
+            op.table.find('.btn-next').addClass('disabled').prop('disabled', true);
         }
         else {
-            op.table.find('.btn-next').removeClass('disabled');
+            op.table.find('.btn-next').removeClass('disabled').prop('disabled', false);
         }
     };
     
@@ -435,9 +435,7 @@
             var $this = $(this);
             var data = $this.data('bs.iconpicker');
             var options = typeof option === 'object' && option;
-            if (!data) {
-                $this.data('bs.iconpicker', (data = new Iconpicker(this, options)));
-            }
+            $this.data('bs.iconpicker', (data = new Iconpicker(this, options)));
             if (typeof option === 'string') {
                 if (typeof data[option] === 'undefined') {
                     throw 'Iconpicker => The "' + option + '" method does not exists.';
@@ -470,7 +468,7 @@
                             trigger: 'manual',
                             html: true,
                             content: op.table,
-                            container: 'body',
+                            container: op.container || 'body',
                             placement: op.placement
                         }).on('shown.bs.popover', function () {
                             data.switchPage(op.icon);
